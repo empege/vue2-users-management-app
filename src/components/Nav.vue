21a852b50a02c0fe707273b345e01d13771d9cee
Ivan Mitov <ivan.mitov@merkle.com>
2023-01-19 15:45:13 +0100

    fix: netlify build path change

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,22 +1,24 @@
 <template>
-  <nav class="nav">
-    <router-link to="/">
-        <div class="nav__logo">
-        <img src="https://clickup.com/images/schema_org/logo.png" alt="logo">
+  <div class="nav__wrapper">
+    <nav class="nav">
+      <router-link to="/">
+          <div class="nav__logo">
+          <img src="https://clickup.com/images/schema_org/logo.png" alt="logo">
+        </div>
+      </router-link>
+      <div class="nav__content">
+        <ul class="nav__items">
+          <transition name="slide-up"><li class="nav__item"><router-link to="/">Users</router-link></li></transition>
+          <transition name="slide-up"><li class="nav__item"><router-link to="/companies">Companies</router-link></li></transition>
+          <transition name="slide-up"><li class="nav__item"><router-link to="/newsletter">Newsletter</router-link></li></transition>
+        </ul>
+        <span class="weather" @click="toggleWeatherPopup">Weather</span>
+        <keep-alive>
+          <component v-bind:is="this.showWeatherPopup && 'weather-popup'" v-on:closePopup="closePopup"></component>
+        </keep-alive>
       </div>
-    </router-link>
-    <div class="nav__content">
-      <ul class="nav__items">
-        <transition name="slide-up"><li class="nav__item"><router-link to="/">Users</router-link></li></transition>
-        <transition name="slide-up"><li class="nav__item"><router-link to="/companies">Companies</router-link></li></transition>
-        <transition name="slide-up"><li class="nav__item"><router-link to="/newsletter">Newsletter</router-link></li></transition>
-      </ul>
-      <span class="weather" @click="toggleWeatherPopup">Weather</span>
-      <keep-alive>
-        <component v-bind:is="this.showWeatherPopup && 'weather-popup'" v-on:closePopup="closePopup"></component>
-      </keep-alive>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
 
 <script>
