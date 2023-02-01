91280af4e96b24fe09eddbf287c2f10ccdb964f8
Ivan Mitov <ivan.mitov@merkle.com>
2023-02-01 09:43:02 +0100

    feature: posts intersection observer

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -11,6 +11,7 @@
     <section v-else>
       <h1>Loading...</h1>
     </section>
+    <div ref="infiniteScrollTrigger" class="list__scroll-trigger">...</div>
   </div>
 </template>
 
@@ -23,16 +24,41 @@ export default {
   data() {
     return {
       page_title: this.$route.name,
-      posts: []
+      posts: [],
+      startFrom: 0,
+      amountPerPage: 5,
+      currentAmount: 5,
+      maxAmount: 100
     }
   },
+  methods: {
+    scrollTrigger() {
+      const observer = new IntersectionObserver((entries) => {
+        console.log('entries', entries)
+        entries.forEach((entry) => {
+          if(entry.intersectionRatio > 0 && this.currentAmount < this.maxAmount) {
+            console.log('vece od nula - znaci ides na dole, prikazuje se nesto sto do sad nije')
+            this.currentAmount += this.amountPerPage
+            this.fetchPosts()
+          }
+        })
+      })
+      observer.observe(this.$refs.infiniteScrollTrigger)
+    },
+    fetchPosts() {
+      fetch(`https://jsonplaceholder.typicode.com/posts?_start=${this.startFrom}&_limit=${this.currentAmount}`)
+        .then(res=>res.json())
+        .then(data => {
+          console.log(data)
+          this.posts = data
+        })
+    }
+  },
+  mounted() {
+    this.scrollTrigger();
+  },
   created() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res=>res.json())
-    .then(data => {
-      console.log(data)
-      this.posts = data
-    })
+    this.fetchPosts();
   }
 }
 </script>
